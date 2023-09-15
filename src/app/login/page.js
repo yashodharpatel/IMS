b3b6629f8aa3247a318ffd81c5566df1b238d411
Rizwan <rizwanjamadar19@gmail.com>
2023-09-15 16:58:23 +0530

    commit registerlogin

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,21 @@
-import React from 'react'
+"use client"
+import React from "react";
+// import styles from "../style";
+import "./login.scss";
+import {webname} from '../../../constant.js'
 
 const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
-export default Login
+  return (
+    <div className="login-container">
+      <div className="login bg-discount-gradient">
+        <h1>Welcome Back to <span className="text-gradient">{webname}</span></h1>
+        <form>
+          <input type="text" placeholder="Username"/>
+          <input type="Password" placeholder="Password"/>
+          <button className="bg-blue-gradient">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Login;
