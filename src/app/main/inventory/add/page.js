85d9d9272d8b86eb2a362b0ad86f11c0a441c821
Rizwan <rizwanjamadar19@gmail.com>
2023-09-16 09:53:38 +0530

    add product

--- src/app/main/inventory/add/page.js
+++ src/app/main/inventory/add/page.js
@@ -1,15 +1,97 @@
-import React from 'react'
-import '../../style.css'
+"use client";
+import React, { useState } from "react";
+import "../../style.css";
+import "./add.css";
 
 const Add = () => {
+  const [product, setProduct] = useState({
+    name: "",
+    category: "",
+    description: "",
+    quantity: "",
+  });
+  const [quantity, setQuantity] = useState(1);
+
+  const handleChange = () => {
+    setProduct({
+      ...product,
+      quantity: quantity,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log(product);
+
   return (
-    <div className='rightbar'>
+    <div className="rightbar">
+      <div className="add-container">
         <h2>Easily Add Your Inventory</h2>
         <form>
-            
+          <div className="input">
+            <label htmlFor="">
+              Inventory Name
+              <span>
+                <sup>*</sup>
+              </span>
+            </label>
+            <input
+              name="name"
+              type="text"
+              placeholder="Acer pc"
+              className="box"
+              required
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input">
+            <label htmlFor="">
+              Category
+              <span>
+                <sup>*</sup>
+              </span>
+            </label>
+            <input
+              name="category"
+              type="text"
+              placeholder="Hardware"
+              className="box"
+              required
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input">
+            <label htmlFor="">
+              Description
+              <span>
+                <sup>*</sup>
+              </span>
+            </label>
+            <input
+              name="description"
+              type="text"
+              placeholder="Acer pc"
+              className="box"
+              required
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input quantity">
+            <button
+              onClick={() => setQuantity((prev) => (prev == 1 ? 1 : prev - 1))}
+            >
+              -
+            </button>
+            {quantity}
+            <button onClick={() => setQuantity((prev) => prev + 1)}> +</button>
+          </div>
+          <button type="submit" className="btn">Add</button>
         </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Add
+export default Add;
